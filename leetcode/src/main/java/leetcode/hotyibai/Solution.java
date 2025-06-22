08196e957352178e2710e6db4a25bdf3ed2c9e5e
tommy <1443137780@qq.com>
2025-06-22 16:56:38 +0800

    hot100题目

--- leetcode/src/main/java/leetcode/hotyibai/Solution.java
+++ leetcode/src/main/java/leetcode/hotyibai/Solution.java
@@ -1,9 +1,7 @@
 package leetcode.hotyibai;
 
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 /**
  * hot100
@@ -223,5 +221,63 @@ public class Solution {
         }
         return s.charAt(i - 1) == p.charAt(j - 1);
     }
+
+    /**
+     * <a href="https://leetcode.cn/problems/container-with-most-water/?envType=problem-list-v2&envId=2cktkvj">盛最多水的容器</a>
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * <a herf="https://leetcode.cn/problems/letter-combinations-of-a-phone-number/?envType=problem-list-v2&envId=2cktkvj">电话号码的字母组合</a>
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() != 0)
+            backtrack("", digits);
+        return output;
+    }
+    Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+    List<String> output = new ArrayList<String>();
+    public void backtrack(String combination, String next_digits) {
+        if (next_digits.length() == 0) {
+            output.add(combination);
+        }
+        else {
+            String digit = next_digits.substring(0, 1);
+            String letters = phone.get(digit);
+            for (int i = 0; i < letters.length(); i++) {
+                String letter = phone.get(digit).substring(i, i + 1);
+                backtrack(combination + letter, next_digits.substring(1));
+            }
+        }
+    }
+
+
 
 }
